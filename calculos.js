e5c28209cf989bcea48ef632b067c2ca722b8132
juanjorato <43244613+juanjorato@users.noreply.github.com>
2020-02-19 16:12:33 -0500

    Se agrega codigo final

--- calculos.js
+++ calculos.js
@@ -1,10 +1,27 @@
 
-
-function clic(){
+function operaciones() {
+    var x = document.getElementById("operation").value;
     var num1 = document.getElementById("nuNumber1").value;
     var num2 = document.getElementById("nuNumber2").value;
-    
-    var resultado =parseFloat(num1)  + parseInt(num2);
-    var inputx = document.getElementById("nuNumber6").value = resultado;
-    console.log (resultado);
+    var rn = document.getElementById("nuNumber6");
+    switch (x) {
+        case "Select":
+            rn.value = "";
+            return;
+        case "Suma":
+            rn.value = parseFloat(num1) + parseInt(num2);
+            break;
+        case "Resta":
+            rn.value = parseFloat(num1) - parseInt(num2);
+            break;
+        case "Multiplicación":
+            rn.value = parseFloat(num1) * parseInt(num2);
+            break;
+        case "Division":
+            rn.value = parseFloat(num1) / parseInt(num2);
+            break;
+
+    }
+
 }
+
